1115fb00f2123a27f5df918da88da1aa7824b9d4
Arokh-web <michaelabudich@gmail.com>
2025-02-22 13:43:24 +0100

    changed style of cards, added add-button, tried usage of LS with object

--- src/Components/PreviewCard.jsx
+++ src/Components/PreviewCard.jsx
@@ -1,22 +1,43 @@
 const PreviewCard = () => {
   // Here is the preview of the journal entry. It reads the data out of the local storage, if there is any.
-  const placeholderImage =
-    "https://i.pinimg.com/736x/2a/11/93/2a1193dabddd7412fb06ddaf78b7f872.jpg";
+
+  const lsData = [
+    {
+      title: "Title of the Entry",
+      date: "Current Date",
+      text: "Text of Entry",
+      img: "https://i.pinimg.com/736x/2a/11/93/2a1193dabddd7412fb06ddaf78b7f872.jpg",
+    },
+    {
+      title: "Title of the Entry",
+      date: "Current Date",
+      text: "Text of Entry",
+      img: "https://i.pinimg.com/736x/2a/11/93/2a1193dabddd7412fb06ddaf78b7f872.jpg",
+    },
+    {
+      title: "Title of the Entry",
+      date: "Current Date",
+      text: "Text of Entry",
+      img: "https://i.pinimg.com/736x/2a/11/93/2a1193dabddd7412fb06ddaf78b7f872.jpg",
+    },
+  ];
+
   return (
     <>
-      <div className="previewCard">
-        <figure>
-          <img src={placeholderImage} alt="preview" className="previewImage" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Date</h2>
-          <p>Title</p>
-          <div className="card-actions justify-end">
-            {/* Maybe the Full view can be seen below this preview-entry?? */}
-            <button className="btn btn-success">View full entry.</button>
+      {lsData.map((entry, index) => (
+        <div className="previewCard" key={index}>
+          <div class="w-2/6">
+            <img src={entry.img} alt="preview" className="previewImage" />
+          </div>
+          <div className="card-body w-4/6">
+            <h2 className="card-title">{entry.date}</h2>
+            <p>{entry.title}</p>
+            <div className="card-actions justify-start">
+              <button className="btn btn-info">View full entry</button>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </>
   );
 };
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import PreviewCard from "./Components/PreviewCard";
-import NewEntry from "./Components/NewModal";
+import AddBar from "./Components/AddBar";
 
 const Home = () => {
   return (
     <div className="main">
       <h1 className="mainH1">Personal Diary</h1>
+      <AddBar />
       <PreviewCard />
-      <NewEntry />
     </div>
   );
 };
