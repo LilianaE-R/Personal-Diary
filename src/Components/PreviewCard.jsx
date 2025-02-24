60c91591a639c9ef04b30d5a019f40d900ddd4fa
Arokh-web <michaelabudich@gmail.com>
2025-02-24 21:00:01 +0100

    improved colors and first preview card fuctionality

--- src/Components/PreviewCard.jsx
+++ src/Components/PreviewCard.jsx
@@ -1,22 +1,30 @@
 const PreviewCard = () => {
   // Here is the preview of the journal entry. It reads the data out of the local storage, if there is any.
 
+  // const [showPreview, setShowPreview] = useState(lsData);
+
+  // useEffect(() => {
+  //   if (localStorage.getItem("journalEntries")) {
+  //     setShowPreview(true);
+  //   }
+  // }, []);
+
   const lsData = [
     {
-      title: "Title of the Entry",
-      date: "Current Date",
+      title: "Title of the first Entry",
+      date: "First Date",
       text: "Text of Entry",
       img: "https://i.pinimg.com/736x/2a/11/93/2a1193dabddd7412fb06ddaf78b7f872.jpg",
     },
     {
-      title: "Title of the Entry",
-      date: "Current Date",
+      title: "Title of the second Entry",
+      date: "Second Date",
       text: "Text of Entry",
       img: "https://i.pinimg.com/736x/2a/11/93/2a1193dabddd7412fb06ddaf78b7f872.jpg",
     },
     {
-      title: "Title of the Entry",
-      date: "Current Date",
+      title: "Title of the third Entry",
+      date: "Third Date",
       text: "Text of Entry",
       img: "https://i.pinimg.com/736x/2a/11/93/2a1193dabddd7412fb06ddaf78b7f872.jpg",
     },
@@ -26,14 +34,14 @@ const PreviewCard = () => {
     <>
       {lsData.map((entry, index) => (
         <div className="previewCard" key={index}>
-          <div class="w-2/6">
+          <div className="w-2/6">
             <img src={entry.img} alt="preview" className="previewImage" />
           </div>
           <div className="card-body w-4/6">
             <h2 className="card-title">{entry.date}</h2>
             <p>{entry.title}</p>
             <div className="card-actions justify-start">
-              <button className="btn btn-info">View full entry</button>
+              <button className="btn btn-outline">View full entry</button>
             </div>
           </div>
         </div>
